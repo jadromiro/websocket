cd29739e06d29f7df960ba9969b1c0e2d8b9f050
jadromiro <jadromiro@gmail.com>
2025-04-03 12:39:03 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,34 +3,23 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
-    console.log("Client connected");
+    console.log('Client connected');
 
     ws.on('message', (message, isBinary) => {
         if (isBinary) {
-            
-
-    if (message.data instanceof Blob) {
-        message.data.arrayBuffer().then((buffer) => {
-            const decoder = new TextDecoder();
-            const text = decoder.decode(new Uint8Array(buffer));
-	
-	console.log("Received binary data:", reader.readAsArrayBuffer(event.data));        
-	console.log("Decoded binary data:", text);
-
-	//console.log("Received binary data:", new Uint8Array(message));
-        // Echo back the binary message
-        ws.send(text, { binary: false });
-
+            console.log('Received binary data:', message);
+            // Echo back the binary message
+            ws.send(message, { binary: true });
         } else {
-            console.log("Received text message:", message.toString());
+            console.log('Received text message:', message.toString());
             // Echo back the text message
-            ws.send(`Echo: ${message}`);
+            ws.send(`Server received: ${message}`);
         }
     });
 
     ws.on('close', () => {
-        console.log("Client disconnected");
+        console.log('Client disconnected');
     });
 });
 
-console.log("WebSocket server running on ws://localhost:8080");
+console.log('WebSocket server is running on ws://localhost:8080');
