e2a62d8c721b85b3102ed42ab37128cc94cd01e7
jadromiro <jadromiro@gmail.com>
2025-04-03 12:21:55 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,23 +3,33 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
-    console.log('Client connected');
+    console.log("Client connected");
 
     ws.on('message', (message, isBinary) => {
         if (isBinary) {
-            console.log('Received binary data:', message);
-            // Echo back the binary message
-            ws.send(message, { binary: true });
+            
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            console.log(new Uint8Array(reader.result)); // Decoded binary
+        };
+       
+	console.log("Received binary data:", reader.readAsArrayBuffer(event.data));
+
+	//console.log("Received binary data:", new Uint8Array(message));
+        // Echo back the binary message
+        ws.send(message, { binary: true });
+
         } else {
-            console.log('Received text message:', message.toString());
+            console.log("Received text message:", message.toString());
             // Echo back the text message
-            ws.send(`Server received: ${message}`);
+            ws.send(`Echo: ${message}`);
         }
     });
 
     ws.on('close', () => {
-        console.log('Client disconnected');
+        console.log("Client disconnected");
     });
 });
 
-console.log('WebSocket server is running on ws://localhost:8080');
+console.log("WebSocket server running on ws://localhost:8080");
