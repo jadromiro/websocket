8ce16d61eb18abc30603dcaed30651b5051e46be
jadromiro <jadromiro@gmail.com>
2025-04-03 13:07:22 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,23 +7,17 @@ wss.on('connection', (ws) => {
 
     ws.on('message', (message, isBinary) => {
         if (isBinary) {
-            
+            // If the message is binary, handle it
+            const text = new TextDecoder().decode(message);
+            console.log("Received binary data:", new Uint8Array(message));
+            console.log("Decoded binary data:", text);
 
-    if (message.data instanceof Blob) {
-        message.data.arrayBuffer().then((buffer) => {
-            const decoder = new TextDecoder();
-            const text = decoder.decode(new Uint8Array(buffer));
-	
-	console.log("Received binary data:", reader.readAsArrayBuffer(message.data));        
-	console.log("Decoded binary data:", text);
-
-	//console.log("Received binary data:", new Uint8Array(message));
-        // Echo back the binary message
-        //ws.send(text, { binary: false });
-        ws.send(`Echo: ${text}`);
-                                        }
+            // Echo back the decoded binary data as text
+            ws.send(`Echo: ${text}`);
         } else {
+            // If the message is text, handle it
             console.log("Received text message:", message.toString());
+
             // Echo back the text message
             ws.send(`Echo: ${message}`);
         }
